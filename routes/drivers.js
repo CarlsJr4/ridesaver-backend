c29a39c33c973b8036487c72eb53e001b11c9a9c
Carl Dungca <carl.dungca@yahoo.com>
2020-07-05 16:36:46 -0700

    Wrote an endpoint to transfer passengers between driver lists

--- routes/drivers.js
+++ routes/drivers.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Drivers } = require('../models/events');
 const driverRouter = express.Router({ mergeParams: true });
 
+// Create a new driver
 driverRouter.post('/', async (req, res) => {
   const event = req.event;
   const newDriver = new Drivers({
@@ -14,6 +15,28 @@ driverRouter.post('/', async (req, res) => {
   res.send(newDriver);
 });
 
+// Transfer passengers between driver lists
+driverRouter.put('/transfer', async (req, res) => {
+  try {
+    const event = req.event;
+    const columnIdStart = '5ef538186635ff06cc86258c';
+    const columnIdEnd = '5ef5386d2cd03b10a4dd4a17';
+    const startColumnPassengers = [];
+    const endColumnPassengers = [
+      { name: 'Carl D', nickname: 'CarlsJr3', _id: '5ef53c4e41f7840ac8e6f135' },
+    ];
+    const startDriver = event.drivers.id(columnIdStart).passengers;
+    const endDriver = event.drivers.id(columnIdEnd);
+    startDriver.passengers = startColumnPassengers;
+    endDriver.passengers = endColumnPassengers;
+    event.save();
+    res.send(event.drivers);
+  } catch {
+    res.send('error');
+  }
+});
+
+// Routes involving making changes to a specific driver
 driverRouter
   .route('/:driver_id')
   .put(async (req, res) => {
@@ -39,5 +62,10 @@ driverRouter
       res.send('driver not found');
     }
   });
+
+// A route to handle re-ordering a specific driver
+// driverRouter.put('/:driver_id/reorder', async (req, res) => {
+//   res.send('foo');
+// });
 
 module.exports = driverRouter;
