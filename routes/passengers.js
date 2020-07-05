6d78027a2d0a89d0c66be6385688d3a3d0358909
Carl Dungca <carl.dungca@yahoo.com>
2020-07-05 16:36:46 -0700

    Wrote an endpoint to transfer passengers between driver lists

--- routes/passengers.js
+++ routes/passengers.js
@@ -4,6 +4,15 @@ const passengerRouter = express.Router({ mergeParams: true });
 // DELETE passenger
 // PUT passenger details
 
+// Need to manage nested subdocuments
+
+// Idea:
+// Get document
+// Get driver subdocument using ID
+// Manually set the array?
+// Or do subdocument methods work for nested subdocuments?
+// When changing the order of things, frontend should send the entire dataset?
+
 // Endpoints for managing specific passengers
 passengerRouter.delete('/:passenger_id', async (req, res) => {
   res.send('foo');
