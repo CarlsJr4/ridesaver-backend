5b3eafbaec1efee33d14fb9dd7a763ef297d3710
Carl Dungca <carl.dungca@yahoo.com>
2022-12-09 00:10:05 -0800

    Update cors options

--- index.js
+++ index.js
@@ -32,8 +32,19 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/events', eventsRouter);
 
+var whitelist = ['http://localhost:3000', 'https://carlsjr4.github.io'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 // Etc
-app.get('/', function (req, res) {
+app.get('/', cors(corsOptions), function (req, res) {
   res.send('Hello World');
 });
 
