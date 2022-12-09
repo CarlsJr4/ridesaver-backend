2f00ce13ec1fda59eeef9e6899e1d817033e9454
Carl Dungca <carl.dungca@yahoo.com>
2022-12-08 23:45:51 -0800

    Moves CORS to top of middleware

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ db.once('open', function () {
 mongoose.set('useFindAndModify', false);
 
 // Middleware
+app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/events', eventsRouter);
